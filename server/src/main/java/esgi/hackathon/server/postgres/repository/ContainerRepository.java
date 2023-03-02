07f8f57c10a6ebd99ac790a5da0a5bf42c95da66
Barlords <basile.pulin@gmail.com>
2023-03-02 18:38:34 +0100

    integration container

--- server/src/main/java/esgi/hackathon/server/postgres/repository/ContainerRepository.java
+++ server/src/main/java/esgi/hackathon/server/postgres/repository/ContainerRepository.java
@@ -1,12 +1,10 @@
 package esgi.hackathon.server.postgres.repository;
 
+import esgi.hackathon.server.postgres.entity.ContainerEntity;
 import esgi.hackathon.server.postgres.entity.StoredProductsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ContainerRepository extends JpaRepository<ContainerEntity, Long> {
