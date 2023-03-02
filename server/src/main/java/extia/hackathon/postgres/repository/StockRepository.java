5d61e32b09ee9ece6eadbe78b8187114fbf94cfc
Sid B <sid.bennaceur@haidi.io>
2023-03-02 15:56:24 +0100

    done

--- server/src/main/java/extia/hackathon/postgres/repository/StockRepository.java
+++ server/src/main/java/extia/hackathon/postgres/repository/StockRepository.java
@@ -11,8 +11,7 @@ import java.util.List;
 @Repository
 public interface StockRepository extends JpaRepository<StocksEntity, Long> {
 
-    List<StocksEntity> findByPlace(String place);
 
-    @Query("SELECT 100 - COUNT(stock.id) FROM STOCKS stock WHERE stock.place = :place")
-    int getAvailablePlacesInStock(@Param("place") String place);
+    @Query("SELECT 100 - COUNT(stock.id) FROM STOCKS stock")
+    int getAvailablePlacesInStock();
 }
