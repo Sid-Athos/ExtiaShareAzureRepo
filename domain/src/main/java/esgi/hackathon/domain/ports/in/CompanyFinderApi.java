0be52ba0ca20afb02c096acecb8762e23f3863d3
Mathieu <ferreira.mathieu.pro@gmail.com>
2023-03-02 20:54:15 +0100

    Select product in company

--- domain/src/main/java/esgi/hackathon/domain/ports/in/CompanyFinderApi.java
+++ domain/src/main/java/esgi/hackathon/domain/ports/in/CompanyFinderApi.java
@@ -2,6 +2,7 @@ package esgi.hackathon.domain.ports.in;
 
 import esgi.hackathon.domain.ApplicationError;
 import esgi.hackathon.domain.functional.model.Company;
+import esgi.hackathon.domain.functional.model.StoredProduct;
 import io.vavr.control.Either;
 import io.vavr.control.Option;
 
@@ -12,5 +13,7 @@ public interface CompanyFinderApi {
     Either<ApplicationError, Company> findById(Long id);
 
     List<Company> findAll();
+
+    List<StoredProduct> findAllByCompany(Long companyId);
 
 }
